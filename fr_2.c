a4a2d57645cc1f2cf7cbd4be806a6a8b16e6461d
YogaVicky <yogasrivarshan@gmail.com>
2019-03-20 07:38:35 +0530

    fr_2.c

--- fr_2.c
+++ fr_2.c
@@ -8,11 +8,17 @@ void swap(int *c,int *d)
 }
 int main()
 {
-    int a,b; 
+    int a,b,c;
+    printf("\nenter the sentinel control\n");
+    while((c=getchar())==5)
+    { 
+    printf("\n%ld\n",main);
     printf("\nenter two numbers\n");
     scanf("%d%d",&a,&b);
     printf("Before swap:\na=%d,b=%d",a,b);
     swap(&a,&b);
+    printf("\n%ld\n",swap);
     printf("\nAfter swap:\na=%d,b=%d\n",a,b);
+    }
     return 0;
 }
